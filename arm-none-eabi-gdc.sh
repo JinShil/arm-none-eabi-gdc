e3798d85ea9a96c89840ace2b1ee256538f8ed7b
Mike <slavo5150@yahoo.com>
2015-01-14 20:10:27 +0900

    encode as UTF-8

--- arm-none-eabi-gdc.sh
+++ arm-none-eabi-gdc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright © 2015 Michael V. Franklin
+# Copyright Â© 2015 Michael V. Franklin
 #
 # This file is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
