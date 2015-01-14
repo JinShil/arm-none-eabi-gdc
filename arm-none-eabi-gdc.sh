c50b87b6e1b7a4ef60045f899b804541ef34389a
Mike <slavo5150@yahoo.com>
2015-01-14 11:53:47 +0900

    Added license

--- arm-none-eabi-gdc.sh
+++ arm-none-eabi-gdc.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# Copyright © 2015 Michael V. Franklin
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this file. If not, see <http://www.gnu.org/licenses/>.
+
+# References
+#------------------------------------------------------------------
 # gcc.gnu.org/install/configure.html
 # http://wiki.dlang.org/GDC/Cross_Compiler/Generic
+# http://wiki.dlang.org/Bare_Metal_ARM_Cortex-M_GDC_Cross_Compiler
 
 # stop if an error is encountered
 set -e
